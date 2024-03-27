c57317097fbe911a3e79842fdbb9009900b19612
bagusgandhi <bagusgandhi4@gmail.com>
2024-03-27 08:30:48 +0700

    feat: data bank constants

---
+++ src/common/constants/bank.ts
@@ -0,0 +1,17 @@
+export const paymentBank = [
+    {
+        name: "bca",
+        image: "https://sample-demo-dot-midtrans-support-tools.et.r.appspot.com/bca.png",
+        checked: true
+    },
+    {
+        name: "bni",
+        image: "https://sample-demo-dot-midtrans-support-tools.et.r.appspot.com/bni.png",
+        checked: false
+    },
+    {
+        name: "bri",
+        image: "https://sample-demo-dot-midtrans-support-tools.et.r.appspot.com/bri.png",
+        checked: false
+    }
+]
