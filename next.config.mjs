84b1fe7f39e1505a3bee1128350cb131afa07e60
bagusgandhi <bagusgandhi4@gmail.com>
2024-03-27 08:14:06 +0700

    feat: add server runtime config environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    serverRuntimeConfig: {
+        APP_ENV: process.env.APP_ENV,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+        MIDTRANS_SERVERKEY: process.env.NEXT_MIDTRANS_SERVERKEY,
+        MIDTRANS_URL: process.env.NEXT_MIDTRANS_URL,
+        SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL
+    },
+    publicRuntimeConfig: {},
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
